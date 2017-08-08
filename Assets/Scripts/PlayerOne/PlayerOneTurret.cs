ade2c3313fa5aebf1ab4ff60937bcd3b5148a638
Thomas Johnson <tjcgjohnson@gmail.com>
2017-08-08 13:27:19 +1000

    turret

--- Assets/Scripts/PlayerOne/PlayerOneTurret.cs
+++ Assets/Scripts/PlayerOne/PlayerOneTurret.cs
@@ -23,11 +23,11 @@ public class PlayerOneTurret : MonoBehaviour
 
     void RotationTurret()
     {
-        if (Input.GetKey(KeyCode.Q))
+        if (Input.GetKey(KeyCode.E))
         {
             transform.Rotate(0, 0,1);
         }
-        if (Input.GetKey(KeyCode.E))
+        if (Input.GetKey(KeyCode.Q))
         {
             transform.Rotate(0, 0, -1);
         }
